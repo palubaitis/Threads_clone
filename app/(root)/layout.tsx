d449e96dc79b777e55c5a799c49d6bd2da2ac62d
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-10-27 23:32:39 +0300

    Implemented main pgae layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import TopBar from "@/components/shared/TopBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import BottomBar from "@/components/shared/BottomBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +22,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <TopBar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-fill max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <BottomBar />
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ components/shared/TopBar.tsx
@@ -0,0 +1,3 @@
+export default function TopBar(){
+    return <h1>TopBar</h1>
+}
