91677c911d5e7323ac5f76aa8a3a9e5971ab7c69
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-10-28 16:54:42 +0300

    Updated utils.ts

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -50,12 +50,12 @@ const AccountProfile = ({ user }: Props, { btnTitle }: Props) => {
     e.preventDefault();
 
     const fileReader = new FileReader();
-    if (e.target.files && e.target.files.length) {
+    if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
       setFiles(Array.from(e.target.files));
       if (!file.type.includes("image")) return;
       fileReader.onload = async (event) => {
-        console.log(event.target?.result?.toString())
+        console.log(event.target?.result?.toString());
         const imageDataUrl = event.target?.result?.toString() || "";
         fieldChange(imageDataUrl);
       };
@@ -64,6 +64,8 @@ const AccountProfile = ({ user }: Props, { btnTitle }: Props) => {
   };
 
   function onSubmit(values: z.infer<typeof UserValidation>) {
+    const blob = values.profile_photo
+    const hasImageChanged = isBase64Image(blob)
     console.log(values);
   }
   return (
