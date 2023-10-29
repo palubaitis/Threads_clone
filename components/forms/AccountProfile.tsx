9976d51e3742d7e1c5c95d8dd8b371309d0b23e1
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-10-29 10:37:44 +0200

    Updating user

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user }: Props, { btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
@@ -74,6 +78,20 @@ const AccountProfile = ({ user }: Props, { btnTitle }: Props) => {
         values.profile_photo = imgRes[0].fileUrl;
       }
     }
+    await updateUser({
+      userId:user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+if(pathname === "/profile/edit"){
+  router.back()
+}else{
+  router.push("/")
+}
+    
   };
   return (
     <Form {...form}>
