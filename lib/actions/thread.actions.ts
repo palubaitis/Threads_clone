fb3728f90ea43838ada87ba73c71b6a09be07d4b
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-11-01 09:37:17 +0200

    implemented fetch posts funcionality

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -18,22 +18,48 @@ export async function createThread({
   communityId,
   path,
 }: Params) {
-    try {
-        connectToDB();
-        const createdThread = await Thread.create({
-          text,
-          author,
-          community: null,
-          path,
-        });
-      
-        await User.findByIdAndUpdate(author, {
-          $push: { threads: createdThread._id },
-        });
-      
-        revalidatePath(path);
-    } catch (error:any) {
-        throw new Error(`Error creating thread: ${error.message}`)
-    }
- 
+  try {
+    connectToDB();
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+      path,
+    });
+
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread: ${error.message}`);
+  }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+const posts = await postsQuery.exec()
+
+const isNext = totalPostsCount > skipAmount + posts.length;
+
+return{posts,isNext }
 }
