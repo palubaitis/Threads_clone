1eaf4139fb0bc81c8573134146b9338a519047ad
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-11-04 10:16:12 +0200

    Implemented thread card styling

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
   id: string;
   currentUserId: string;
@@ -27,12 +30,68 @@ const ThreadCard = ({
   comments,
 }: Props) => {
   return (
-    <article>
-      <h2 className="text-small-regular text-light-2">
-        {content}
-      </h2>
+    <article className="flex flex-col w-full p-1 rounded-xl bg-dark-2 p-7">
+      <div className="flex items-start justify-between">
+        <div className="flex flex-row flex-1 w-full gap-4">
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+              <Image
+                src={author.image}
+                alt="Profile image"
+                fill
+                className="rounded-full cursor-pointer"
+              />
+            </Link>
+            <div className="thread-card_bar" />
+          </div>
+          <div className="flex flex-col w-full">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-base-semibold text-light-1">
+                {author.name}
+              </h4>
+            </Link>
+            <p className="mt-2 text-small-regular text-light-2">{content}</p>
+            <div className="flex flex-col gap-3 mt-5">
+              <div className="flex gap-3.5">
+                <Image
+                  src="/assets/heart-gray.svg"
+                  alt="heart"
+                  width={24}
+                  height={24}
+                  className="object-contain cursor-pointer"
+                />
+                <Link href={`/thread/${id}`}>
+                  {" "}
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="reply"
+                    width={24}
+                    height={24}
+                    className="object-contain cursor-pointer"
+                  />
+                </Link>
+
+                <Image
+                  src="/assets/repost.svg"
+                  alt="repost"
+                  width={24}
+                  height={24}
+                  className="object-contain cursor-pointer"
+                />
+                <Image
+                  src="/assets/share.svg"
+                  alt="share"
+                  width={24}
+                  height={24}
+                  className="object-contain cursor-pointer"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
 
-export default ThreadCard; 
+export default ThreadCard;
