bec0a6c0aabf45480846e2b2fa7c94c93bdb29df
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-11-06 19:53:46 +0200

    T

--- next.config.js
+++ next.config.js
@@ -1,30 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-      serverComponents: true, // Use 'serverComponents' instead of 'serverComponentsExternalPackages'
+   experimental: {
+    serverActions: true,
+      serverComponents: true, 
     },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
