36718d7131f6ac7b7f61afb38b32d8d5463ccaca
Strike7777 <110612063+Strike7777@users.noreply.github.com>
2023-11-13 15:25:52 +0200

    Implemented full fetchThreadById funcionality

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -69,10 +69,34 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
 export async function fetchThreadById(id: string) {
   connectToDB();
   try {
-    const thread = await Thread.findById(id).populate({
-      path: "author",
-      model: User,
-      select: "_id id name image",
-    });
-  } catch (error) {}
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image ",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+  }
 }
